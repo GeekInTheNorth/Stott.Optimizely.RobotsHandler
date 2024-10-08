900803ceaf50d173db4275b0c5649a14bf4133df
Mark Stott <mypantsareontheoutside@gmail.com>
2024-10-08 22:31:28 +0100

    Test with different robots options.

---
+++ Sample/OptimizelyTwelveTest/Features/Attributes/DisallowRobotsActonFilterAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace OptimizelyTwelveTest.Features.Attributes;
+
+public sealed class DisallowRobotsActonFilterAttribute : ActionFilterAttribute
+{
+    public override void OnActionExecuting(ActionExecutingContext context)
+    {
+        base.OnActionExecuting(context);
+
+        context.HttpContext.Response.Headers.TryAdd("X-Robots-Tag", "noindex, nofollow");
+    }
+}
