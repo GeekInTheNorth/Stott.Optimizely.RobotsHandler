8513c15a3cb61a7fb9d1949262f8b4921a587b4e
Mark Stott <mypantsareontheoutside@gmail.com>
2025-08-26 16:54:20 +0100

    Correct permissions for the discover api

--- src/Stott.Optimizely.RobotsHandler/Opal/OpalApiController.cs
+++ src/Stott.Optimizely.RobotsHandler/Opal/OpalApiController.cs
@@ -31,67 +31,60 @@ public sealed class OpalApiController : BaseApiController
 
     [HttpGet]
     [Route("/stott.robotshandler/opal/discovery/")]
-    [OpalAuthorization(OpalAuthorizationLevel.Read)]
     public IActionResult Discovery()
     {
         var authorizationLevel = HttpContext.Items[RobotsConstants.OpalAuthorizationLevelKey] as OpalAuthorizationLevel? ?? OpalAuthorizationLevel.None;
         var model = new FunctionsRoot { Functions = new List<Function>() };
 
-        if (authorizationLevel >= OpalAuthorizationLevel.Read)
+        model.Functions.Add(new Function
         {
-            model.Functions.Add(new Function
+            Name = "getrobottxtconfigurations",
+            Description = "Get a collection of robot.txt configurations optionally filtered by host name.",
+            Parameters = new List<FunctionParameter>
             {
-                Name = "getrobottxtconfigurations",
-                Description = "Get a collection of robot.txt configurations optionally filtered by host name.",
-                Parameters = new List<FunctionParameter>
+                new FunctionParameter
                 {
-                    new FunctionParameter
-                    {
-                        Name = "hostName",
-                        Type = "string",
-                        Description = "The host name to filter the robot.txt configurations by.",
-                        Required = false
-                    }
-                },
-                Endpoint = "/tools/get-robot-txt-configurations/",
-                HttpMethod = "POST"
-            });
-        }
+                    Name = "hostName",
+                    Type = "string",
+                    Description = "The host name to filter the robot.txt configurations by.",
+                    Required = false
+                }
+            },
+            Endpoint = "/tools/get-robot-txt-configurations/",
+            HttpMethod = "POST"
+        });
 
-        if (authorizationLevel >= OpalAuthorizationLevel.Write)
+        model.Functions.Add(new Function
         {
-            model.Functions.Add(new Function
+            Name = "saverobottxtconfigurations",
+            Description = "Saves robots content for a specific Id or Host Name. Where an update is being performed for a host name and a specific configuration does not exist, one will be created.",
+            Parameters = new List<FunctionParameter>
             {
-                Name = "saverobottxtconfigurations",
-                Description = "Saves robots content for a specific Id or Host Name. Where an update is being performed for a host name and a specific configuration does not exist, one will be created.",
-                Parameters = new List<FunctionParameter>
+                new FunctionParameter
                 {
-                    new FunctionParameter
-                    {
-                        Name = "robotsId",
-                        Type = "Guid",
-                        Description = "The Id of the robots.txt configuration that is to be updated.",
-                        Required = false
-                    },
-                    new FunctionParameter
-                    {
-                        Name = "hostName",
-                        Type = "string",
-                        Description = "The host name of of the robots.txt configuration that is to be updated.",
-                        Required = false
-                    },
-                    new FunctionParameter
-                    {
-                        Name = "RobotsTxtContent",
-                        Type = "string",
-                        Description = "The robots.txt content to be saved.  This should be a valid robots.txt content and must be provided.",
-                        Required = true
-                    }
+                    Name = "robotsId",
+                    Type = "Guid",
+                    Description = "The Id of the robots.txt configuration that is to be updated.",
+                    Required = false
                 },
-                Endpoint = "/tools/save-robot-txt-configurations/",
-                HttpMethod = "POST"
-            });
-        }
+                new FunctionParameter
+                {
+                    Name = "hostName",
+                    Type = "string",
+                    Description = "The host name of of the robots.txt configuration that is to be updated.",
+                    Required = false
+                },
+                new FunctionParameter
+                {
+                    Name = "RobotsTxtContent",
+                    Type = "string",
+                    Description = "The robots.txt content to be saved.  This should be a valid robots.txt content and must be provided.",
+                    Required = true
+                }
+            },
+            Endpoint = "/tools/save-robot-txt-configuration/",
+            HttpMethod = "POST"
+        });
 
         return CreateSafeJsonResult(model);
     }
